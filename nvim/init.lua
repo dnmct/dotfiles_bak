3b60fb6ae372d8b879840b3b9a40a4ff27d8ec02
Dan McAtee <mcatee@sciendis.de>
2020-12-28 07:12:09 +0100

    add command for putting in co author

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 require 'settings'
 require 'plugins'
 require 'keybinds'
+require 'commands'
 
 require 'plugins/_coc'
 require 'plugins/_fzf'
-
---
+++ nvim/lua/commands.lua
@@ -0,0 +1,1 @@
+vim.cmd([[command! -nargs=+ Gca :r!git log -n100 --pretty=format:"\%an <\%ae>" | grep -i '<args>' | head -1 | xargs echo "Co-authored-by:"]])
