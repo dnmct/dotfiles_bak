3f515f7a5a6ff3d37783600e95d0d8fc34d7908d
dnmct <mcatee@sciendis.de>
2021-05-10 22:57:41 +0200

    first draft at new vim config

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+require('settings')
+require('plugins')
+require('keymaps')
+
+-- LSP
+require('lsp.lua-lsp')
+
+-- Plugins
+require('configs.nvimtree')
+require('configs.bufferline')
+require('configs.comment')
+require('configs.completion')
+
+require('zephyr')
---
+++ nvim/lua/configs/bufferline/init.lua
@@ -0,0 +1,5 @@
+require('bufferline').setup{}
+
+-- Tab switch buffer
+vim.api.nvim_set_keymap('n', '<Tab>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
